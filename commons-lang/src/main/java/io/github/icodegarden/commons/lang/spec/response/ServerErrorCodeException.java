f7aac98b36931b72b35dd30062540ab09716b01c
徐芳方 <Fangfang.Xu@geely.com>
2022-05-16 09:56:59 +0800

    自动config spring

--- commons-lang/src/main/java/io/github/icodegarden/commons/lang/spec/response/ServerErrorCodeException.java
+++ commons-lang/src/main/java/io/github/icodegarden/commons/lang/spec/response/ServerErrorCodeException.java
@@ -1,5 +1,12 @@
 package io.github.icodegarden.commons.lang.spec.response;
 
+import java.lang.reflect.Method;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.util.ClassUtils;
+
 /**
  * 
  * @author Fangfang.Xu
@@ -8,7 +15,29 @@ package io.github.icodegarden.commons.lang.spec.response;
 public class ServerErrorCodeException extends ErrorCodeException {
 	private static final long serialVersionUID = 1L;
 
+	private static final Logger log = LoggerFactory.getLogger(ServerErrorCodeException.class);
+
 	private static String applicationName = "NotConfig";
+
+	static {
+		if (ClassUtils.isPresent("io.github.icodegarden.commons.springboot.SpringContext",
+				ClassUtils.getDefaultClassLoader())) {
+			try {
+				Class<?> springContextClass = ClassUtils.forName(
+						"io.github.icodegarden.commons.springboot.SpringContext", ClassUtils.getDefaultClassLoader());
+				Method getApplicationContextMethod = springContextClass.getDeclaredMethod("getApplicationContext");
+				Object applicationContext = getApplicationContextMethod.invoke(null);
+				Class<?> applicationContextClass = ClassUtils.forName("org.springframework.context.ApplicationContext",
+						ClassUtils.getDefaultClassLoader());
+				Method getBeanMethod = applicationContextClass.getMethod("getBean", Class.class);
+				Environment env = (Environment) getBeanMethod.invoke(applicationContext, Environment.class);
+				String applicationName = env.getRequiredProperty("spring.application.name");
+				ServerErrorCodeException.configApplicationName(applicationName);
+			} catch (Exception e) {
+				log.error("WARN ex on init configApplicationName", e);
+			}
+		}
+	}
 
 	public static final String CODE = "20000";
 	public static final String MSG = "Service Currently Unavailable";
@@ -24,7 +53,7 @@ public class ServerErrorCodeException extends ErrorCodeException {
 	public ServerErrorCodeException(String error_point, Throwable cause) {
 		super(CODE, MSG, String.format("server.%s.%s-error", applicationName, error_point), MSG, cause);
 	}
-	
+
 	public ServerErrorCodeException(String error_point, String sub_msg, Throwable cause) {
 		super(CODE, MSG, String.format("server.%s.%s-error", applicationName, error_point), sub_msg, cause);
 	}
