e904f2fbfae5f2520a44c8718ec3de94c373ce0a
徐芳方 <Fangfang.Xu@geely.com>
2022-10-10 10:45:05 +0800

    AESUtils

---
+++ commons-lang/src/test/java/io/github/icodegarden/commons/lang/util/AESUtilsTests.java
@@ -0,0 +1,28 @@
+package io.github.icodegarden.commons.lang.util;
+
+import java.util.UUID;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ * @author Fangfang.Xu
+ *
+ */
+class AESUtilsTests {
+
+	@Test
+	void encrypt_decrypt() throws Exception {
+		String data = UUID.randomUUID().toString();
+		String aeskey = UUID.randomUUID().toString();
+		
+		String encrypt = AESUtils.encrypt(data, aeskey);//结果是16进制字符串
+		System.out.println("encrypt:"+encrypt);
+		String decrypt = AESUtils.decrypt(encrypt, aeskey);
+		System.out.println("decrypt:"+decrypt);
+		
+		Assertions.assertThat(decrypt).isEqualTo(data);
+	}
+
+}
