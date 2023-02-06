12a6691498659ad1b3b59edc2caa4442c6f182f9
徐芳方 <Fangfang.Xu@geely.com>
2023-02-06 16:38:21 +0800

    update

--- commons-gateway/src/main/java/io/github/icodegarden/commons/gateway/spi/impl/DefaultOpenApiRequestValidator.java
+++ commons-gateway/src/main/java/io/github/icodegarden/commons/gateway/spi/impl/DefaultOpenApiRequestValidator.java
@@ -30,12 +30,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class DefaultOpenApiRequestValidator implements OpenApiRequestValidator {
 
-	/**
-	 * 可配
-	 */
-	public static long REJECT_SECONDS_BEFORE = 5 * 60;
-	public static long REJECT_SECONDS_AFTER = 10;
-	public static Pattern DATETIME_PATTERN = Pattern.compile("^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$");
+	private static final long REJECT_SECONDS_BEFORE = 5 * 60;
+	private static final long REJECT_SECONDS_AFTER = 10;
+	private static final Pattern DATETIME_PATTERN = Pattern.compile("^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$");
 
 	private GeneralValidator generalValidator = new GeneralValidator();
 	private RequestIdValidator requestIdValidator = new RequestIdValidator();
