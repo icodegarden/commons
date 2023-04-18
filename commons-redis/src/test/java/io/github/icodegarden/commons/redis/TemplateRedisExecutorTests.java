304dcca71baff262c8bb2bd9534494c1a018231f
徐芳方 <Fangfang.Xu@geely.com>
2023-04-18 09:18:31 +0800

    update version

--- commons-redis/src/test/java/io/github/icodegarden/commons/redis/TemplateRedisExecutorTests.java
+++ commons-redis/src/test/java/io/github/icodegarden/commons/redis/TemplateRedisExecutorTests.java
@@ -6,6 +6,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import redis.clients.jedis.JedisPoolConfig;
@@ -25,12 +26,13 @@ public class TemplateRedisExecutorTests extends RedisExecutorTests {
 
 	public static RedisTemplate<String, Serializable> newRedisTemplate() {
 		RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<String, Serializable>();
-		redisTemplate.setConnectionFactory(connectionFactory());
+//		redisTemplate.setConnectionFactory(jedisConnectionFactory());//二选一
+		redisTemplate.setConnectionFactory(lettuceConnectionFactory());//二选一
 		redisTemplate.afterPropertiesSet();
 		return redisTemplate;
 	}
 
-	static RedisConnectionFactory connectionFactory() {
+	static RedisConnectionFactory jedisConnectionFactory() {
 		JedisPoolConfig poolConfig = new JedisPoolConfig();
 		JedisClientConfiguration clientConfig = JedisClientConfiguration.builder().usePooling().poolConfig(poolConfig)
 				.build();
@@ -43,5 +45,16 @@ public class TemplateRedisExecutorTests extends RedisExecutorTests {
 		JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(redisConfig, clientConfig);
 		jedisConnectionFactory.afterPropertiesSet();// 需要调用一下，不然pool不会生效，内部总是创建一个新链接
 		return jedisConnectionFactory;
+	}
+	
+	static RedisConnectionFactory lettuceConnectionFactory() {
+		RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+		redisConfig.setHostName("172.22.122.23");
+//		redisConfig.setPassword(RedisPassword.of("8q9P&ZF5SQ@Fv49x"));
+		redisConfig.setPort(6399);
+
+		LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisConfig);
+		lettuceConnectionFactory.afterPropertiesSet();// 需要调用一下，不然pool不会生效，内部总是创建一个新链接
+		return lettuceConnectionFactory;
 	}
 }
